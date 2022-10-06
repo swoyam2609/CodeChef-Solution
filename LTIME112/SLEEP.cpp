5982f2e6316c5b2eae9a482d98e82464f95591f5
Anirudh Das <anirudh.das2903@outlook.in>
2022-10-06 16:11:05 +0530

    Create SLEEP.cpp
    Submitted the cpp file..

---
+++ LTIME112/SLEEP.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n<7)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
