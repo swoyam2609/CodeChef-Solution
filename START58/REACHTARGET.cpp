b151ac5c36335d0d101bd6d37cc3abacabbd29bb
Anirudh Das <anirudh.das2903@outlook.in>
2022-10-06 16:19:18 +0530

    Create REACHTARGET.cpp
    Submitted the cpp file.

---
+++ START58/REACHTARGET.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int m,n;
+	    cin>>m>>n;
+	    cout<<abs(m-n)<<endl;
+	    
+	}
+	return 0;
+}
