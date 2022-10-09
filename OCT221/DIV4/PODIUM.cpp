e00d10159160c3d59ffd4b65dfd65f51f40350d0
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-10-10 01:18:36 +0530

    Added .gitignore file and 3 Solutions for OCT long Challenge

---
+++ OCT221/DIV4/PODIUM.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define testCase int t; cin >> t; while (t--)
+using namespace std;
+
+int main()
+{
+    testCase
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
