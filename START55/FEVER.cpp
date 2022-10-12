a4d7bd11e2a44b4bbe869d2ce08cb42707fcd239
sidharth2603 <115610556+sidharth2603@users.noreply.github.com>
2022-10-12 11:24:17 +0530

    Create FEVER.cpp

---
+++ START55/FEVER.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        
+        int x;
+        cin>>x;
+        
+        if(x>98){
+            cout<<"YES"<<endl;
+            
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
