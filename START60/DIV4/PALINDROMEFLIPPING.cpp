20c71de9c5ccf44ae06c4b56f183f81394f4b90e
js313 <91676294+js313@users.noreply.github.com>
2022-10-13 11:19:02 +0530

    Palindrome flipping solution added (START60/DIV4)

---
+++ START60/DIV4/PALINDROMEFLIPPING.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, nz = 0, no = 0;
+        cin >> n;
+        string arr;
+        cin >> arr;
+        for (int j = 0; j < arr.length(); j++)
+        {
+            if (arr[j] == '0')
+                nz++;
+            else
+                no++;
+        }
+        if (n % 2 == 0)
+        {
+            if (nz % 2 == 0 && no % 2 == 0)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+        else
+        {
+            if (nz % 2 == 0 && no % 2 == 0)
+                cout << "NO\n";
+            else
+                cout << "YES\n";
+        }
+    }
+    return 0;
+}
