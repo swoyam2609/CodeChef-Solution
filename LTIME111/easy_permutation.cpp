2b1317ee5df28d186a2b01f7fc1e0b921a86d374
Rajat Gupta <54673939+rajat5559@users.noreply.github.com>
2022-10-25 11:16:15 +0530

    Create easy_permutation.cpp

---
+++ LTIME111/easy_permutation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int i,a;
+	    cin>>a;
+	    for(i=a;i>=1;i--)
+	    cout<<i<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
