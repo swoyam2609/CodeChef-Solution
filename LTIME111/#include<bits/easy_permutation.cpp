58454a3941e64d1b75738d0d3ee6f0b50b8f3ab0
Shreya Mishra <91959996+Shreya-Lu@users.noreply.github.com>
2022-10-28 05:55:49 +0530

    Create easy_permutation.cpp
    Added solution for LTime111 @ Easy Permutation

---
+++ LTIME111/#include<bits/easy_permutation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,i;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(i=n;i>=1;i--){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
